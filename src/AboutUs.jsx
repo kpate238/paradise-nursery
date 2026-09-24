6a92584ab65eca5ab362750ae78588b21bd50836
Kush Patel <kpate238@charlotte.edu>
2026-09-24 17:19:56 -0400

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        At Paradise Nursery, we bring nature closer to your home. Our mission is to provide healthy, vibrant, and sustainable indoor plants that elevate your living space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
